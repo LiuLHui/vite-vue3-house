9400e249aa221af3a554f4b5a58189c9f67e887d
LiuLHui <410777647@qq.com>
2020-12-02 10:09:02 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { provide, ref } from 'vue';
+export default {
+  name: "App",
+  setup(){
+    const asideVisible = ref(false)
+    provide('xxx',asideVisible)  // set
+  }
+};
+</script>
 <style lang="scss" scoped>
   
 </style>
-<script>
-export default {
-  name: "App",
-};
-</script>
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,23 +1,36 @@
 <template>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+  <div class="topnav">
+    <div class="logo" @click="toggleAside">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import { inject, Ref } from "vue";
 export default {
-    name:'Topnav'
-}
+  name: "Topnav",
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("xxx"); // get
+    console.log(asideVisible.value);
+    const toggleAside = () => {
+      asideVisible.value = !asideVisible.value;
+    };
+    return {
+      toggleAside,
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
 .topnav {
   background: pink;
   display: flex;
   padding: 16px;
+  position: relative;
+  z-index: 10;
   > .logo {
     max-width: 6em;
     margin-right: auto;

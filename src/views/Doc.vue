bd83b7be0d9daa84b8fd2fa314479aa0cd3b5820
LiuLHui <410777647@qq.com>
2020-12-02 10:24:42 +0800

    topnav完成

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -31,11 +31,10 @@ export default {
     Topnav,
   },
   setup(){
-  const asideVisible = inject<Ref<boolean>>('xxx') // get 
-  console.log(asideVisible.value)
-  return {
-    asideVisible
-  }
+    const asideVisible = inject<Ref<boolean>>('asideVisible') // get 
+    return {
+      asideVisible
+    }
   }
 };
 </script>
@@ -44,10 +43,6 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top:70px;
   > h2 {
     margin-bottom: 4px;
   }
@@ -55,6 +50,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width:500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top:70px;
   }
 }
 </style>

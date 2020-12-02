16e15da85c7dee131bc96b1096079ea62dbd015e
LiuLHui <410777647@qq.com>
2020-12-02 09:49:54 +0800

    初始化aside

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,19 +6,41 @@
       <h1>XXXXXXXXXXXXXXX</h1>
       <p class="actions">
         <a>github</a>
-        <a>开始</a>
+        <router-link to='/doc'>开始</router-link>
       </p>
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-
-</style>
-<script >
-import Topnav from '../components/Topnav.vue'
+<style lang="scss" scoped></style>
+<script>
+import Topnav from "../components/Topnav.vue";
 export default {
-  components:{
-    Topnav
+  components: {
+    Topnav,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
   }
 }
-</script>
+</style>
